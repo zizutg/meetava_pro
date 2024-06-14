27e87d707919fe1ed893ace0de027673a7e025d6
Yalew Zelalem Jembre <zizutg@gmail.com>
2024-06-14 00:23:07 -0400

    proper fonts added, account details class model and provider completed, consumer widget to follow

--- lib/src/scripts/get_file_names.dart
+++ lib/src/scripts/get_file_names.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() {
-  final directory = Directory('../../../assets/fonts/');
+  final directory = Directory('../../../assets/');
   directory.list().forEach((FileSystemEntity entity) {
     if (entity is File) {
       print(entity.path.split('/').last);
